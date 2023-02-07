95c48cabbc1772a83797bdc59e4687de7601e5d0
Gabriel Barbosa <59917720+gabrielb-ss@users.noreply.github.com>
2023-02-06 22:29:26 -0300

    change 'font-size' ratio for easier px to rem conversion

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,19 +1,21 @@
 import { createGlobalStyle } from "styled-components"
 
 export const GlobalStyle = createGlobalStyle`
+
   * {
+    border: 0px solid black;
     margin: 0;
     padding: 0;
     box-sizing: border-box;
     font-family: 'Poppins', sans-serif;
+    color: rgba(255, 255, 255, 0.8);
   }
 
   html {
-        @media (max-width: 1080px) {
-            font-size: 93.75%;
-        }
-        @media (max-width: 720px) {
-            font-size: 87.5%;
-        }
-    }
+    font-size: 62.5%;
+  }
+
+  body {
+    background-color: #190E2F;
+  }
 `
