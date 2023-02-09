688202a41b3e4bef61fd638ec469867b1d3fb140
Gabriel Barbosa <59917720+gabrielb-ss@users.noreply.github.com>
2023-02-09 15:03:56 -0300

    create style for Aside component

---
+++ src/components/Aside/styles.ts
@@ -0,0 +1,34 @@
+import styled from 'styled-components'
+
+export const Content = styled.div`
+
+  background: rgba(0, 0, 0, 0.33);
+  
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  
+  .aside-icon {
+    padding: 3rem;
+    display: flex;
+    width: 100%;
+    max-height: 5rem;
+    align-items: center;
+    justify-content: center;
+    border-bottom: 1px solid rgba(255, 255, 255, 0.33);
+  }
+
+  .aside-content {
+    padding: 6rem 4.4rem;
+    overflow-y:auto;
+    display: flex;
+    flex-direction: column;
+  }
+
+  .aside-content.text {
+    text-align: justify;
+  }
+  
+`
+
+  
