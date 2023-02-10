6c5207923f67b836ccba0776058c0d38b1666d3e
Gabriel Barbosa <59917720+gabrielb-ss@users.noreply.github.com>
2023-02-09 21:25:52 -0300

    Add function to render content of Aside based of type

--- src/components/Aside/index.tsx
+++ src/components/Aside/index.tsx
@@ -2,6 +2,7 @@ import { Content } from './styles';
 import featuresImg from '../../assets/features.svg';
 import toolsImg from '../../assets/tools.svg';
 
+
 type AsideProps = {
   type: string,
   features: {
@@ -11,18 +12,34 @@ type AsideProps = {
   }
 }
 
+function Item(type: string, features: string[], tools: string[]) {
+  if (type === "features") {
+    return features.map((feat, index) => <li key={index}>{feat}</li>)
+
+  } else if (type === "tools") {
+    return tools.map((toolIcon, index) => 
+      <li key={index}>
+        <img src={`/icons/${toolIcon}.svg`} alt="" />
+      </li>
+    )
+  }
+}
+
+
 export function Aside ({type, features:{tools, description, feats}}: AsideProps) {
+  
+
   return (
    
     <Content>
       <div className="aside-icon">
-        <img src={type === "features" ? featuresImg : toolsImg } alt="" />
+        <img src={type === "features" ? featuresImg : toolsImg } alt="aside-icon" />
       </div>
     
-      <div className="aside-content">
-        <p>{description}</p>
+      <div className={"aside-content "}>
+        <p>{type === "features" ? description : ''}</p>
         <ul>
-            {feats.map((feat, index) => <li key={index}>{feat}</li>)}
+           {Item(type, feats, tools)}
         </ul>
       </div> 
     </Content>
