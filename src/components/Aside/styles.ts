75d0b6b5cabbe2c94a931190f8e7fea9a424f8d3
Gabriel Barbosa <59917720+gabrielb-ss@users.noreply.github.com>
2023-02-09 22:30:59 -0300

    update styles for each Aside type

--- src/components/Aside/styles.ts
+++ src/components/Aside/styles.ts
@@ -1,38 +1,57 @@
 import styled from 'styled-components'
 
 export const Content = styled.div`
-
+  width: 18vw;
   background: rgba(0, 0, 0, 0.33);
   
   display: flex;
   flex-direction: column;
   align-items: center;
-  
-  p {
-    margin-bottom: 3rem;
-  }
 
   .aside-icon {
-    padding: 3rem;
-    display: flex;
     width: 100%;
     max-height: 5rem;
+    padding: 3rem;
+
+    display: flex;
     align-items: center;
     justify-content: center;
+
     border-bottom: 1px solid rgba(255, 255, 255, 0.33);
   }
 
   .aside-content {
+    width: 100%;
     padding: 6rem 4.4rem;
     overflow-y:auto;
-    display: flex;
-    flex-direction: column;
   }
 
-  .aside-content.text {
+  .aside-content.features {
+    p {
+     margin-bottom: 3rem;
+    }
+
     text-align: justify;
   }
-  
+
+  .aside-content.tools {
+    ul {
+      list-style: none;
+
+      li {
+        display: flex;
+        justify-content: center;
+        
+        img {
+          width: 6.5rem;
+        }
+      }
+
+      li + li {
+        margin-top: 5.5rem;
+      }
+    }
+  }
 `
 
   
