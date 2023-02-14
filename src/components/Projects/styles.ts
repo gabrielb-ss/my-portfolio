75c668e695d91360a7f2433a8420d2b2d0575bdd
Gabriel Barbosa <59917720+gabrielb-ss@users.noreply.github.com>
2023-02-14 17:32:58 -0300

    refactor component Projects' CSS

--- src/components/Projects/styles.ts
+++ src/components/Projects/styles.ts
@@ -1,23 +1,8 @@
 import styled from 'styled-components';
-import { Content as NavBar } from '../ProjectsNavBar/styles';
 
 export const Container = styled.div`
-  
   width: 100vw;
-  height: 59.4vh;
-  margin-top: 4rem;
-  display: grid;
-
-  grid-template-columns: 1.8fr 6.4fr 1.8fr;
-  grid-template-rows: 9fr 1fr;
-
-  grid-template-areas: 
-  "aside1 slide aside2"
-  "aside1 slide aside2"
-  "aside1 navbar aside2";
-  
-  ${NavBar} {
-    grid-area: navbar;
-  }
-  
+  height: 60vh;
+  margin-top: 8rem;
+  display: flex;
 `
