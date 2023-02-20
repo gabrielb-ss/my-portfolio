ceafb275fc84ca8a1d2f1ce481d37d3c8411c6bd
Gabriel Barbosa <59917720+gabrielb-ss@users.noreply.github.com>
2023-02-19 22:47:35 -0300

    feat(Aside): add responsiveness

--- src/components/Aside/styles.ts
+++ src/components/Aside/styles.ts
@@ -1,8 +1,9 @@
 import styled from 'styled-components'
 
 export const Content = styled.div`
-  width: 18vw;
   height: inherit;
+  width: 18vw;
+
   background: rgba(0, 0, 0, 0.33);
   
   display: flex;
@@ -11,20 +12,20 @@ export const Content = styled.div`
 
   .aside-icon {
     width: 100%;
-    max-height: 5rem;
+    height: 5rem;
     padding: 3rem;
 
     display: flex;
     align-items: center;
     justify-content: center;
-
+    
     border-bottom: 1px solid rgba(255, 255, 255, 0.33);
   }
 
   .aside-content {
     width: 100%;
     padding: 6rem 4.4rem;
-    overflow-y:auto;
+    overflow-y: auto;
   }
 
   .aside-content.features {
@@ -38,10 +39,14 @@ export const Content = styled.div`
     }
 
     text-align: justify;
-    font-size: 3rem;
+    font-size: 2rem;
   }
 
   .aside-content.tools {
+    p {
+      display: none;
+    }
+
     ul {
       list-style: none;
 
@@ -56,6 +61,68 @@ export const Content = styled.div`
 
       li + li {
         margin-top: 5.5rem;
+      }
+    }
+  }
+
+  @media (max-width: 750px) {
+    width: 100vw;
+    height: fit-content;
+    padding-bottom: 2rem;
+
+    .aside-icon {
+      padding: 0;
+      border: none;
+      border-bottom: 1px solid rgba(255, 255, 255, 0.33);
+
+      img {
+        height: 3rem;
+        width: 3rem;
+        object-fit: contain;
+      }
+    }
+
+    .aside-content {
+      * {
+        //outline: red solid 1px;
+      }
+
+      padding: 0;
+      margin: 0;
+
+      align-items: center;
+      justify-content: center;
+
+      ul {
+        display: flex;
+        flex-wrap: wrap;
+        align-items: center;
+        justify-content: center;
+        gap: 2rem;
+      }
+    }
+    
+    .aside-content.features {
+      p {
+        font-size: medium;
+        text-align: center;
+        margin-top: 1rem;
+      }
+
+      li {
+        font-size: medium;
+      }
+    }
+
+    .aside-content.tools {
+      margin-top: -2rem;
+      ul > li:first-child {
+          transform: translate(0, 50%);
+      }
+
+      ul > li > img {
+        width: 10vw;
+        height: 10vw;
       }
     }
   }
