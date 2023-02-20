7126f5090ef533743c3ab98cb628eab5a2b23822
Gabriel Barbosa <59917720+gabrielb-ss@users.noreply.github.com>
2023-02-19 22:49:47 -0300

    feat(Header): add responsiveness to component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,55 +2,23 @@ import { Container } from './styles'
 import logoImg from '../../assets/logo.png'
 import sunImg from '../../assets/sun.svg'
 import moonImg from '../../assets/moon.svg'
-import projectsImg from '../../assets/projects.svg'
-import aboutmeImg from '../../assets/user.svg'
-import contactImg from '../../assets/contact.svg'
-import { useState } from 'react';
 
 export function Header () {
-  const [activeTab, setActiveTab] = useState([0,1,0]);
-
-  function selectTab(id: number) {
-    let newActiveTab = [0,0,0];
-    newActiveTab[id] = 1;
-    setActiveTab(newActiveTab);
-  }
-
   return (
     <Container> 
         <img id='gb-logo' src={logoImg} alt="Gabriel Barbosa"/>
 
-        <nav>
-          <a onClick={() => selectTab(0)} className={activeTab[0] === 1 ? "active" : "" }>
-            <img src={aboutmeImg} alt="" />
-          </a>
-          <a onClick={() => selectTab(1)} className={activeTab[1] === 1 ? "active" : "" }>
-            <img src={projectsImg} alt="" />
-          </a>
-          <a onClick={() => selectTab(2)} className={activeTab[2] === 1 ? "active" : "" }>
-            <img src={contactImg} alt="" />
-          </a>
-        </nav>
+        <h1>MY PROJECTS</h1>
 
-        <div id='switchers-container'>
-          <div className='switcher' >
-            <img src={sunImg} alt="" />
+        <div className='switcher' >
+          <img src={sunImg} alt="" />
 
-            <input type="checkbox" id="mode" />
-            <label htmlFor="mode"></label>
+          <input type="checkbox" id="mode" />
+          <label htmlFor="mode"></label>
 
-            <img src={moonImg} alt="" />
-          </div>
-
-          <div className='switcher'>
-            <p>PT</p>
-
-            <input type="checkbox" id="lang" />
-            <label htmlFor="lang"></label>
-            
-            <p>EN</p>
-          </div>
+          <img src={moonImg} alt="" />
         </div>
+        
     </Container>
   );
 }
